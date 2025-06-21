38681638e88f80aab599afaf2ab1d801c5234f32
Miriam Frenay <miriam.frenay@gmail.com>
2025-06-21 15:20:30 +0200

    Add support for all Minecraft versions from 1.20 to 1.21.6

--- build.cmd
+++ build.cmd
@@ -1,6 +1,4 @@
 :: Get project name
 for %%d in (.) do set "PROJECT=%%~nxd"
 :: Zip data pack
-tar caf %PROJECT%.zip pack.mcmeta LICENSE.txt data
-:: Optionally zip resource pack
-if exist resources tar caf %PROJECT%_resources.zip --strip-components=1 resources
+tar caf %PROJECT%.zip pack.mcmeta LICENSE.txt data *-overlay
--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 # Get project name
 if [ "$BITBUCKET_REPO_SLUG" != "" ]; then PROJECT="$BITBUCKET_REPO_SLUG"; else PROJECT="${PWD##*/}"; fi
 # Zip data pack
-zip -r - pack.mcmeta LICENSE.txt data > ${PROJECT}.zip
-# Optionally zip resource pack
-[ -d resources ] && ( cd resources && zip -r - * ) > ${PROJECT}_resources.zip
+zip -r - pack.mcmeta LICENSE.txt data *-overlay > ${PROJECT}.zip
 # Return success status for Bitbucket pipeline
 true
